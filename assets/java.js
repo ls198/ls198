30ffd222b0c12dd2343c15e2f960c9107a76c55e
ls198 <81174307+ls198@users.noreply.github.com>
2021-06-15 14:00:37 +0200

    Hamburger

---
+++ assets/java.js
@@ -0,0 +1,18 @@
+/* wait till website is loaded */
+document.addEventListener('DOMContentLoaded', websiteLoaded);
+
+function websiteLoaded() {
+  console.log('Website loaded');
+
+  /* Select the menu */
+  const menu = document.querySelector('#menu');
+
+  /* add click function to the menu */
+  menu.onclick = function () {
+    console.log('menu clicked');
+
+    /* toggle class on our navbar */
+    document.querySelector('.nav-bar').classList.toggle('mobile-active');
+    document.querySelector('body').classList.toggle('mobile-active');
+  };
+}
